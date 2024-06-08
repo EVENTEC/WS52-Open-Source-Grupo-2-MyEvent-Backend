58712973104eb0926d20b2287d3fa1ba3c073a6f
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2024-06-07 22:13:01 -0500

    feat: user command services

--- src/main/java/backend/myevent/MyEvent/news/domain/services/UserCommandService.java
+++ src/main/java/backend/myevent/MyEvent/news/domain/services/UserCommandService.java
@@ -6,4 +6,8 @@ import java.util.Optional;
 
 public interface UserCommandService {
     Optional<User> handle(CreateUserCommand command);
+    void handleDeleteUser(String name, String password);
+    void handleChangeUserPassword(Long id, String correo, String newPassword);
+    void handleChangeUserName(Long id, String correo, String currentPassword, String newName);
+
 }
