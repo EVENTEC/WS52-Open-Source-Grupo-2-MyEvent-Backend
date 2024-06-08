17ad6c6f32345d82f3a38edba9a02e8a817862ee
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2024-06-07 22:14:23 -0500

    feat: user entity assembler

--- src/main/java/backend/myevent/MyEvent/news/interfaces/rest/transform/UserResourceFromEntityAssembler.java
+++ src/main/java/backend/myevent/MyEvent/news/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -2,16 +2,26 @@ package backend.myevent.MyEvent.news.interfaces.rest.transform;
 
 import backend.myevent.MyEvent.news.domain.model.aggregates.User;
 import backend.myevent.MyEvent.news.interfaces.rest.resources.UserResource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserResourceFromEntityAssembler{
 
     public static UserResource toResourceFromEntity(User entity) {
         return new UserResource(
-            entity.getName(),
-            entity.getSurname(),
-            entity.getAddress(),
-            entity.getPhone(),
-            entity.getCorreo(),
-            entity.getPassword(),
-            entity.getNewsApiKey());
+                entity.getName(),
+                entity.getSurname(),
+                entity.getAddress(),
+                entity.getPhone(),
+                entity.getCorreo(),
+                entity.getPassword(),
+                entity.getNewsApiKey());
+    }
+
+    public static List<UserResource> toResourceFromEntityList(List<User> entities) {
+        return entities.stream()
+                .map(UserResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
     }
 }
